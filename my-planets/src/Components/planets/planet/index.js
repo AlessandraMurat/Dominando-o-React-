619952f9bc189e127a2110f15c091d23ca2e1d0a
Alessandra Murat <alessandra.murat@outlook.com>
2022-02-11 12:41:56 -0300

    Criando estados

--- my-planets/src/Components/planets/planet/index.js
+++ my-planets/src/Components/planets/planet/index.js
@@ -3,18 +3,6 @@ import GrayImg from "../../shared/gray-img";
 import DescriptionWithLink from "../../shared/DescriptionWithLink";
 
 const Planet = (props) => {
-  /* método 1 renderizando fora do jsx
-    const names = ['a', 'b', 'c', 'd', 'e', 'f']
-    const satellites = names.map((n) => 
-
-        <li>Satélite {n} </li>
-    )
-
-    */
-
-  // método 2 renderizando dentro do jsx
-  const names = ["a", "b", "c", "d", "e", "f"];
-
   let title;
   if (props.title_with_underline)
     title = (
@@ -29,19 +17,6 @@ const Planet = (props) => {
       {title}
       <DescriptionWithLink description={props.description} link={props.link} />
       <GrayImg img_url={props.img_url} gray={props.gray} />
-      <h4>Satélites</h4>
-
-      {/* método 1
-            <ul>{satellites}</ul> */}
-
-            
-      {/* método 2  */}
-      <ul>
-        {names.map((n) => (
-          <li>Satélite {n} </li>
-        ))}
-      </ul>
-
       <hr />
     </div>
   );
