c22e34486370a4219bbf44cebaba5ecb2b0bb056
Alessandra Murat <alessandra.murat@outlook.com>
2022-02-13 10:57:42 -0300

    lifecycle

--- my-planets/src/Components/planets/index.js
+++ my-planets/src/Components/planets/index.js
@@ -1,37 +1,18 @@
 import React, { Fragment } from "react";
 import Planet from "./planet";
 
+async function getPlanets() {
+  let response = await fetch('http://localhost:3000/api/planets.json') 
+  let data = await response.json()
+  return data;
+}
+
 class Planets extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      planets: [
-        {
-          title: "Mercúrio",
-          description:
-            "Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas. ",
-          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
-          img_url:
-            "https://www.mapadomeuceu.com.br/wp-content/uploads/2020/10/planeta-mercurio-pr%C3%B3ximo-ao-sol.jpg",
-        },
-        {
-          title: "Júpiter",
-          description:
-            "Júpiter é o maior planeta do Sistema Solar, tanto em diâmetro quanto em massa, e é o quinto mais próximo do Sol.[11] Possui menos de um milésimo da massa solar, contudo tem 2,5 vezes a massa de todos os outros planetas em conjunto. ",
-          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
-          img_url:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkbOKIrtPLCnK8gYPjYSAiS4GSWqotqW9FLA&usqp=CAU",
-        },
-        {
-          title: "Júpiter",
-          description:
-            "Júpiter é o maior planeta do Sistema Solar, tanto em diâmetro quanto em massa, e é o quinto mais próximo do Sol.[11] Possui menos de um milésimo da massa solar, contudo tem 2,5 vezes a massa de todos os outros planetas em conjunto. ",
-          link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)",
-          img_url:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkbOKIrtPLCnK8gYPjYSAiS4GSWqotqW9FLA&usqp=CAU",
-        },
-      ],
+      planets: []
     };
   }
 
@@ -50,6 +31,14 @@ class Planets extends React.Component {
     }))
   }
 
+  componentDidMount() {
+    getPlanets().then(data => {
+      this.setState(state => ({
+        planets: data['planets']
+      }))
+    })
+  }
+
   render() {
     return (
       <Fragment>
@@ -57,12 +46,14 @@ class Planets extends React.Component {
         <button onClick={this.removeLast}>Remove Last</button>
         <button onClick={this.duplicatePlanet}>Duplicate Last Planet</button>
         <hr />
-        {this.state.planets.map((planet) => (
+        {this.state.planets.map((planet, index) => (
           <Planet
+            id = {planet.id}
             title={planet.title}
             description={planet.description}
             link={planet.link}
             img_url={planet.img_url}
+            key = {index}
           />
         ))}
       </Fragment>
--- my-planets/src/Components/planets/planet/index.js
+++ my-planets/src/Components/planets/planet/index.js
@@ -2,24 +2,58 @@ import React from "react";
 import GrayImg from "../../shared/gray-img";
 import DescriptionWithLink from "../../shared/DescriptionWithLink";
 
-const Planet = (props) => {
-  let title;
-  if (props.title_with_underline)
-    title = (
-      <h4>
-        <u>{props.title}</u>
-      </h4>
-    );
-  else <h4>{props.title}</h4>;
+async function getSatellites(id) {
+  let response = await fetch(`http://localhost:3000/api/${id}.json`);
+  let data = await response.json();
+  return data;
+}
 
-  return (
-    <div>
-      {title}
-      <DescriptionWithLink description={props.description} link={props.link} />
-      <GrayImg img_url={props.img_url} gray={props.gray} />
-      <hr />
-    </div>
-  );
-};
+class Planet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      satellites: [],
+    };
+  }
+
+  componentDidMount() {
+    getSatellites(this.props.id).then(data => {
+      this.setState(state => ({
+        satellites: data["satellites"]
+      }));
+    });
+  }
+
+  render() {
+    let title;
+    if (this.props.title_with_underline)
+      title = (
+        <h4>
+          <u>{this.props.title}</u>
+        </h4>
+      );
+    else <h4>{this.props.title}</h4>;
+
+    return (
+      <div>
+        {title}
+        <DescriptionWithLink
+          description={this.props.description}
+          link={this.props.link}
+        />
+        <GrayImg img_url={this.props.img_url} gray={this.props.gray} />
+        <hr />
+        <h4>Satélites</h4>
+        <ul>
+          {this.state.satellites.map((satellite, index) => 
+            <li key = {index}>
+              {satellite.name}
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Planet;
